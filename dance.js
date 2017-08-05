75bfc8cb9305a40c6ee4c37b26fae82a86a552b8
royschuyler <royschuyler@yahoo.com>
2017-08-05 13:46:26 -0700

    initial commit - just dance

---
+++ dance.js
@@ -0,0 +1,348 @@
+var finalCount = 0;
+
+function makeDancer(d,tPeak,start,start2,t1,t2,t3,t4,f,v,size){
+  var rad90Use = radians(90)/d;
+  var gap = t1-t4;
+  var topDif = t1-t2;
+  var btmDif = t3-t4;
+  var topRate = topDif/gap;
+  var btmRate = btmDif/gap;
+  var bPeak = tPeak*btmRate;
+  var topUse = (tPeak*topRate)/d;
+  var btmUse = (tPeak*btmRate)/d;
+  var topTarr =  tNumbers(d,tPeak,topUse);
+  var btmTarr = tNumbers(d,bPeak,btmUse);
+  var f1 = f;
+  var v1 = v;
+  var vm1 = cot(radians(v1));
+  var fm1 = cot(radians(90)-t1);
+  var fb1 = tan(t1)*abs(f1);
+  var ix1 = (fb1-v1)/(vm1-fm1);
+  var iy1 = vm1*((fb1-v1)/(vm1-fm1))+ v1;
+  var y1 = (tan(t1)*f1)-iy1;
+  var yLength1 = (tan(t1)*f1);
+  var a1;
+  if(t1>0){
+    a1 = abs(ix1/y1);
+  }else{
+    a1 = abs(ix1/y1) * -1;
+  }
+  var s1 = abs(ix1);
+  var cy1 = iy1;
+  var f2 = f;
+  var v2 = v;
+  var vm2 = cot(radians(v2));
+  var fm2 = cot(radians(90)-t2);
+  var fb2 = tan(t2)*abs(f2);
+  var ix2 = (fb2-v2)/(vm2-fm2);
+  var iy2 = vm2*((fb2-v2)/(vm2-fm2))+ v2;
+  var y2 = (tan(t2)*f2)-iy2;
+  var a2;
+  if(t2>0){
+    a2 = abs(ix2/y2);
+  }else{
+    a2 = abs(ix2/y2) * -1;
+  }
+  var s2 = abs(ix2);
+  var cy2 = iy2;
+  var f3 = f;
+  var v3 = v;
+  var vm3 = cot(radians(v3));
+  var fm3 = cot(radians(90)-t3);
+  var fb3 = tan(t3)*abs(f3);
+  var ix3 = (fb3-v3)/(vm3-fm3);
+  var iy3 = vm3*((fb3-v3)/(vm3-fm3))+ v3;
+  var y3 = (tan(t3)*f3)-iy3;
+  var a3;
+  if(t3>0){
+    a3 = abs(ix3/y3);
+  }else{
+    a3 = abs(ix3/y3) * -1;
+  }
+  var s3 = abs(ix3);
+  var cy3 = iy3;
+  var f4 = f;
+  var v4 = v;
+  var vm4 = cot(radians(v4));
+  var fm4 = cot(radians(90)-t4);
+  var fb4 = tan(t4)*abs(f4);
+  var ix4 = (fb4-v4)/(vm4-fm4);
+  var iy4 = vm4*((fb4-v4)/(vm4-fm4))+ v4;
+  var y4 = (tan(t4)*f4)-iy4;
+  var yLength4 = (tan(t4)*f4);
+  var yLength = (yLength1-yLength4)/2;
+  var a4;
+  if(t4>0){
+    a4 = abs(ix4/y4);
+  }else{
+    a4 = abs(ix4/y4) * -1;
+  }
+  var s4 = abs(ix4);
+  var cy4 = iy4;
+  var radUse = radians(360)/d;
+  var realUse = radUse/4;
+  var e2 = 1/a2;
+  var e1 = 1/a1;
+  var e3 = 1/a3;
+  var e4 = 1/a4;
+  var num = numbers (0, d, 1)
+  var obj1 = basePoints(d,start,a1,s1,cy1);
+  var obj2 = basePoints(d,start,a2,s2,cy2);
+  var objv = basePointsV(d,obj1,obj2);
+  var obj3 = basePoints(d,start2,a3,s3,cy3);
+  var obj4 = basePoints(d,start2,a4,s4,cy4);
+  var objv2 = basePointsV(d,obj3,obj4);
+
+  var obj = {
+    big: points(obj1,d,start,tPeak,topTarr,yLength,yLength1),
+    small: points(obj2,d,start,tPeak,topTarr,yLength,yLength1),
+    vertical: points(objv,d,start,tPeak,topTarr,yLength,yLength1)
+  }
+
+  var obj2 = {
+    big: points(obj3,d,start2,tPeak,btmTarr,yLength,yLength1),
+    small: points(obj4,d,start2,tPeak,btmTarr,yLength,yLength1),
+    vertical: points(objv2,d,start2,tPeak,btmTarr,yLength,yLength1)
+  }
+
+  var buffer = 'newbuffer' + '</br>';
+  var text = '';
+  var minus = size*(yLength1-yLength);
+
+  function plot(side,x1,y1,x2,y2,size,minus){
+     //1 = white
+     //0 = black
+
+     //var first = 0>1; blue > purple > red
+     // var second = 0;
+     // var third = .5;
+
+     // var first = 0>1; bluegreen >  gray > peachred
+     // var second = .5;
+     // var third = .5;
+
+     // var first = 0>1; green >  green > lightyellow
+     // var second = 1;
+     // var third = .5;
+
+     // var first = 1;
+     // var second = 1; bold yellow
+     // var third = .1;
+
+
+
+     var color1;
+     var color2;
+     var color3;
+     // var first = .1;
+     // var second = 0;
+     // var third = .9;
+
+     if(side == 1){
+       color1 = .1;
+       color2 = 0;
+       color3 = .9;
+     }
+     if(side == 2){
+       color1 = .9;
+       color2 = .9;
+       color3 = .9;
+     }
+     if(side == 3){
+       color1 = 1;
+       color2 = 1;
+       color3 = .1;
+     }
+     if(side == 4){
+       color1 = .1;
+       color2 = .1;
+       color3 = .1;
+     }
+     if(side == 5){
+       color1 = 1;
+       color2 = 1;
+       color3 = 1;
+     }
+
+     for(i=0;i<x1.length;i++){
+        buffer += 'newbuffer' + '</br>';
+        text += 'addvalue ' + finalCount + ' ' + x1[i]*size + ' ' + (y1[i]*size-minus)+ '</br>';
+        text += 'addvalue ' + finalCount + ' ' + x2[i]*size + ' ' + (y2[i]*size-minus) + '</br>';
+        text += 'bcolor ' + color1 + ' ' + color2 + ' ' + color3 + ' ' + finalCount + ' ' + 1 + '</br>'
+        finalCount++
+     }
+  }
+
+  if(t1>=0 && t2>=0){
+    if(start>=315 || start<45){
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+    }
+    if(start>=45 && start<135){
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+    }
+    if(start>=135 && start<225){
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+    }
+    if(start>=225 && start<315){
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+    }
+  }
+
+  if(t1<=0 && t2<=0){
+    if(start>=315 || start<45){
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+    }
+    if(start>=45 && start<135){
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+    }
+    if(start>=135 && start<225){
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+    }
+    if(start>=225 && start<315){
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+    }
+  }
+
+  if(t1>=0 && t2<=0){
+    if(start>=315 || start<45){
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+    }
+    if(start>=45 && start<135){
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+    }
+    if(start>=135 && start<225){
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+    }
+    if(start>=225 && start<315){
+      plot(2,obj.vertical.x2,obj.vertical.y2,obj.vertical.x3,obj.vertical.y3,size,minus);
+      plot(3,obj.vertical.x3,obj.vertical.y3,obj.vertical.x4,obj.vertical.y4,size,minus);
+      plot(4,obj.vertical.x4,obj.vertical.y4,obj.vertical.x1,obj.vertical.y1,size,minus);
+      plot(1,obj.vertical.x1,obj.vertical.y1,obj.vertical.x2,obj.vertical.y2,size,minus);
+    }
+  }
+
+
+  if(t3>=0 && t4>=0){
+    if(start2>=315 || start2<45){
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+    }
+    if(start2>=45 && start2<135){
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+    }
+    if(start2>=135 && start2<225){
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+    }
+    if(start2>=225 && start2<315){
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+    }
+  }
+
+  if(t3<=0 && t4<=0){
+    if(start2>=315 || start2<45){
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+    }
+    if(start2>=45 && start2<135){
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+    }
+    if(start2>=135 && start2<225){
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+    }
+    if(start2>=225 && start2<315){
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+    }
+  }
+
+  if(t3>=0 && t4<=0){
+    if(start2>=315 || start2<45){
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+    }
+    if(start2>=45 && start2<135){
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+    }
+    if(start2>=135 && start2<225){
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+    }
+    if(start2>=225 && start2<315){
+      plot(2,obj2.vertical.x2,obj2.vertical.y2,obj2.vertical.x3,obj2.vertical.y3,size,minus);
+      plot(3,obj2.vertical.x3,obj2.vertical.y3,obj2.vertical.x4,obj2.vertical.y4,size,minus);
+      plot(4,obj2.vertical.x4,obj2.vertical.y4,obj2.vertical.x1,obj2.vertical.y1,size,minus);
+      plot(1,obj2.vertical.x1,obj2.vertical.y1,obj2.vertical.x2,obj2.vertical.y2,size,minus);
+    }
+  }
+
+  var end = buffer + text;
+  ////console.log(end)
+  return end
+}
+
+
+
+
+
+
---
+++ endlib.js
@@ -0,0 +1,57 @@
+function bounceArr(bounce,d,peak){
+  var use = ((peak*4)/(d))*bounce;
+  var arr = [];
+  var length = ((d/2)/bounce);
+  for(j=0;j<bounce;j++){
+    var k=0;
+    for(i=0;i<length;i++){
+      arr.push(-peak+(k*use));
+      k++
+    }
+    var k=0;
+    for(i=0;i<length;i++){
+      arr.push(peak-(use*k));
+      k++
+    }
+  }
+  arr.push(-peak)
+  return arr
+}
+
+function regulate(d,topTwist){
+  var arr = [];
+  for(i=0;i<d;i++){
+    if(topTwist*i<=360){
+      arr.push(topTwist*i);
+    }
+    if(topTwist*i>360){
+      var multiplier = Math.floor((topTwist*i/360) % 360);
+      arr.push((topTwist*i)-(360*multiplier))
+    }
+  }return arr
+}
+
+function burst(burstStart,high1,low1,high2,burstD){
+  var arr = [];
+  var burstN = burstD/10;
+  var dUp = (burstD/2)-burstN;
+  var dDown = (burstN/4)*3;
+  var dEnd = (burstN/4)*5;
+  var burtUse1 = (high1-burstStart)/dUp;
+  var restUse1 = (high1-low1)/dDown;
+  var burtUse2 = (high2-low1)/dUp;
+  var restUse2 = (high2-burstStart)/dEnd;
+  for(i=0;i<dUp;i++){
+    arr.push(burstStart+(i*burtUse1));
+  }
+  for(i=0;i<dDown;i++){
+    arr.push(high1-(i*restUse1));
+  }
+  for(i=0;i<dUp;i++){
+    arr.push(low1+(i*burtUse2));
+  }
+  for(i=0;i<dEnd;i++){
+    arr.push(high2-(i*restUse2));
+  }return arr
+}
+
